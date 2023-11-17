7e2bfd5f5fa1e83b401f6f5f6b54252da22d4fdf
rubengersons <rubengersons@gmail.com>
2023-11-17 15:22:16 +0100

    added small angle approx function and command

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -22,10 +22,10 @@ def cmd_group():
     show_default=True
 )
 def sin(number):
-    """Gets the sin values from 0 to 2 pi over [number] steps
+    """Gets the sin values from 0 to 2 pi over a [number] of steps
 
     Args:
-        number (int): the number of steps between 0 and 2 pi
+        number (int): the number of equally spaced steps between 0 and 2 pi
     """
     x = np.linspace(0, 2 * pi, int(number))
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
@@ -41,15 +41,33 @@ def sin(number):
     show_default=True
 )
 def tan(number):
-    """Gets the cos values from 0 to 2 pi over [number] steps
+    """Gets the cos values from 0 to 2 pi over a [number] of steps
 
     Args:
-        number (int): the number of steps between 0 and 2 pi
+        number (int): the number of equally spaced steps between 0 and 2 pi
     """
     x = np.linspace(0, 2 * pi, int(number))
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
     return
+
+@cmd_group.command()
+@click.argument("error")
+def approx(error):
+    """Find the maximum angle for which the sin approximation holds at a given accuracy
+
+    Args:
+        error (float): the accuracy for which to find the maximum angle
+    Returns:
+        float: the maximum angle for which the approximation holds given the accuracy
+    """
+    x = 0
+
+    while abs(x - np.sin(x)) < float(error):
+        x += 0.001
+
+    print(f"For an accuracy of {error}, the small-angle approximation holds up to x = {np.round(x, 3)}.")
+    return x
 
 if __name__ == "__main__":
     cmd_group()
