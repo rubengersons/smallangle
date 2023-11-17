35e0c9fefa9a73eeb9f71b350dce651d90326112
rubengersons <rubengersons@gmail.com>
2023-11-17 14:58:18 +0100

    added command line and docstrings

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -1,22 +1,55 @@
+"""
+Small angle approximation
+
+Ruben Gersons
+"""
+
 import click
 import numpy as np
 from numpy import pi
 import pandas as pd
 
+@click.group()
+def cmd_group():
+    pass
 
+@cmd_group.command()
+@click.option(
+    "-n",
+    "--number",
+    default=10,
+    help="Number of times",
+    show_default=True
+)
 def sin(number):
-    x = np.linspace(0, 2 * pi, number)
+    """Gets the sin values from 0 to 2 pi over [number] steps
+
+    Args:
+        number (int): the number of steps between 0 and 2 pi
+    """
+    x = np.linspace(0, 2 * pi, int(number))
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
     return
 
-
+@cmd_group.command()
+@click.option(
+    "-n",
+    "--number",
+    default=10,
+    help="Number of times",
+    show_default=True
+)
 def tan(number):
-    x = np.linspace(0, 2 * pi, number)
+    """Gets the cos values from 0 to 2 pi over [number] steps
+
+    Args:
+        number (int): the number of steps between 0 and 2 pi
+    """
+    x = np.linspace(0, 2 * pi, int(number))
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
     return
 
-
 if __name__ == "__main__":
-    sin(10)
+    cmd_group()
